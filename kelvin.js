092790375023b3663d995dda11cf892e0235dbe5
Kunal Ranjan <kunalinfo@gmail.com>
2017-12-06 00:23:03 +0530

    Update kelvin.js

--- kelvin.js
+++ kelvin.js
@@ -1,5 +1,5 @@
-kelvin = 294; // 294 kelvin temperature is assigned to variable kelvin.
-let celsius = kelvin - 273; // celsius = kelvin - 273
+const kelvin = 294; // 294 kelvin temperature is assigned to variable kelvin.
+const celsius = kelvin - 273; // celsius = kelvin - 273
 let fahrenheit = celsius * (9/5) + 32; // celsius to fahrenheit
 fahrenheit = Math.floor(fahrenheit); // used .floor() method from math libraray to round the fahrenheit temperature and reassign the value in fahrenheit variable.
 console.log(`The temperature is ${fahrenheit} degrees fahrenheit.`);
